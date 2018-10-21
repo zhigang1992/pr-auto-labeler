355213bb345027a0aaaae9234fd6baf7491c1c84
Kyle Fang <zhigang1992@gmail.com>
2018-10-21 11:15:31 +0800

    Add custom message support

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,23 @@ import { Application } from 'probot'
 import getConfig from 'probot-config'
 
 export = (app: Application) => {
+  app.log('App Loaded')
   app.on(['pull_request.opened', 'pull_request.edited'], async (context) => {
+    context.log('Event triggered')
     const config = await getConfig(context, 'pr-title.yml');
     if (config && typeof config.regex === 'string') {
-      app.log(config.regex)
-      app.log(context.payload.pull_request)
+      context.log(config.regex)
+      const title: string = context.payload.pull_request.title;
+      if ((new RegExp(config.regex)).test(title)) {
+        context.log('No action needed')
+        return;
+      }
+      const comment = context.issue({
+        body: config.message || "Please correct your PR title"
+      })
+      context.github.issues.createComment(comment)
     } else {
-      app.log('config file not found')
+      context.log('config file not found')
     }
-
   });
 }
