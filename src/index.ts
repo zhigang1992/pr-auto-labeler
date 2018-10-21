a6cfe022ece66bc70df27537145cb97fafce8a34
Kyle Fang <zhigang1992@gmail.com>
2018-10-21 11:31:32 +0800

    Fix correction

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,17 @@ import getConfig from 'probot-config'
 export = (app: Application) => {
   app.log('App Loaded')
   app.on(['pull_request.opened', 'pull_request.edited'], async (context) => {
-    context.log('Event triggered')
     const config = await getConfig(context, 'pr-title.yml');
     if (config && typeof config.regex === 'string') {
-      context.log(config.regex)
-      const title: string = context.payload.pull_request.title;
-      const pullRequest = context.payload.pullRequest;
+      const pullRequest = context.payload.pull_request;
+      const title: string = pullRequest.title;
       const checkOptions: any = {
         name: "PR-Title",
         head_branch: '',
         head_sha: pullRequest.head.sha,
         status: 'in_progress',
         output: {
-          title: 'Work in progress',
+          title: config.message || 'PR-Title does not meet requirement',
           summary: config.message || `The title "${pullRequest.title}" meet requirement.`,
           text: ''
         }
@@ -28,7 +26,6 @@ export = (app: Application) => {
         checkOptions.completed_at = new Date()
         checkOptions.output.title = 'Ready for review'
         checkOptions.output.summary = 'PR title now meet requirement'
-        return;
       }
       await context.github.checks.create(context.repo(checkOptions))
     } else {
