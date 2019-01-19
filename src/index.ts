a6c4fb51c554e0297be7aaf2120d947757131103
Kyle Fang <zhigang1992@gmail.com>
2019-01-19 08:32:41 +0800

    Remove deprecated API

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import { Application } from "probot";
 
-interface Label {
-  id: string;
-  name: string;
-}
-
 export = (app: Application) => {
   app.log("App Loaded");
   app.on(
     ["pull_request.opened", "pull_request.edited", "pull_request.synchronize"],
     async context => {
-      // @ts-ignore
-      const labels: Label[] = (await context.github.issues.getLabels(
+      const labels = (await context.github.issues.listLabelsForRepo(
         context.issue({ per_page: 20 })
+      )).data;
+
+      const existingLabels = (await context.github.issues.listLabelsOnIssue(
+        context.issue()
       )).data;
 
       const pullRequest = context.payload.pull_request;
       const title: string = pullRequest.title;
-      const existingLabelIds = (pullRequest.labels as Label[]).map(l => l.id);
+      const existingLabelIds = existingLabels.map(l => l.id);
 
-      const labelToAdd = labels.filter((label: Label) => {
+      const labelToAdd = labels.filter(label => {
         return (
           !existingLabelIds.includes(label.id) && title.includes(label.name)
         );
